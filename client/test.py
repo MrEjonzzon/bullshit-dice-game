363c22edb433ca5905a78b3fa46bcbaf049a8e40
ThunsOJ <oscar_jidaker@icloud.com>
2023-02-26 00:57:08 +0100

    la till chattfunktion

---
+++ client/test.py
@@ -0,0 +1,31 @@
+import time
+from threading import Thread
+
+from client import Client
+
+name = input("Skriv namn: ")
+c1 = Client(name)
+
+
+def update_messages():
+    msgs = []
+    run = True
+    while run:
+        time.sleep(0.1)  # uppdaterar varje 1/10 av sekund
+        new_messages = c1.get_messages()  # hämtar meddelanden
+        msgs.extend(new_messages)  # lägger till i en lista
+
+        for msg in new_messages:  # skriver ut meddelanderna från listan
+            print(msg)
+
+            if msg == "{quit}":
+                run = False
+                break
+
+
+Thread(target=update_messages).start()
+
+while True:
+    message = input()
+    c1.send_message(message)
+
--- server/server.py
+++ server/server.py
@@ -2,44 +2,73 @@ import socket
 import threading
 import time
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+from person import *
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 5500
+ADDR = (HOST, PORT)
+MAX_CONNETIONS = 10
+BUFSIZ = 512
 
-
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected")
-
-    connected = True
-
-    while connected:
-        message_length = conn.recv(HEADER).decode(FORMAT)
-        if message_length:
-            message_length = int(message_length)
-            msg = conn.recv(message_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f"[{addr} {msg}]")
-            conn.send("Message recieved".encode(FORMAT))
-
-    conn.close()
+persons = []
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.bind(ADDR)  # sätter up servern
 
 
-def start():
-    server.listen()
+def broadcast(msg, name):
+    for person in persons:
+        client = person.client
+        try:
+            client.send(bytes(name, "utf8") + msg)
+        except Exception as e:
+            print("[EXCEPTION]", e)
+
+
+def client_communication(person):
+    client = person.client
+
+    # första meddelandet är alltid en människas namn
+    name = client.recv(BUFSIZ).decode("utf8")
+    person.set_name(name)
+
+    msg = bytes(f"{name} has joined the chat!", "utf8")
+    broadcast(msg, "")  # broadcast welcome message
+
+    while True:  # lyssnar efter meddelanden
+        msg = client.recv(BUFSIZ)
+
+        if msg == bytes("{quit}", "utf8"):  # dissconnectar om användaren skriver quit
+            client.close()
+            persons.remove(person)
+            broadcast(bytes(f"{name} has left the chat...", "utf8"), "")
+            print(f"[DISCONNECTED] {name} disconnected")
+            break
+        else:  # otherwise send message to all other clients
+            broadcast(msg, name+": ")
+            print(f"{name}: ", msg.decode("utf8"))
+
+
+def wait_for_connection():
+
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        try:
+            client, addr = SERVER.accept()  # väntar på clienter
+            person = Person(addr, client)  # skapar ett person objekt vid anslutningen
+            persons.append(person)
+
+            print(f"[CONNECTION] {addr} connected to the server at {time.time()}")
+            threading.Thread(target=client_communication, args=(person,)).start()
+        except Exception as e:
+            print("[EXCEPTION]", e)
+            break
+
+    print("SERVER CRASHED")
 
 
-print("Startar server")
-start()
+if __name__ == "__main__":
+    SERVER.listen(MAX_CONNETIONS)  # öppnar servern för att lyssna efter anslutningar
+    print("[STARTED] Waiting for connections...")
+    ACCEPT_THREAD = threading.Thread(target=wait_for_connection)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
